229b2c59528cea096856163f9052ee282262bb77
Carter Clark <crtrclark@gmail.com>
2021-08-06 07:22:56 -0500

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return <div className="App">
+    <h1>CRUD APP</h1>
+
+    <input type="text" name="movieName" />
+    <input type="text" name="review" />
+  </div>
+  ;
+}
+
+export default App;
