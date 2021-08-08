1b7ee83f2e7438a971b13f899823d8a4251c77f3
Carter Clark <crtrclark@gmail.com>
2021-08-08 09:45:35 -0500

    finished project

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,18 @@ import Axios from 'axios'
 
 function App() {
 
-  var index = 4;
+  var index = 1;
 
-  const[movie_name, setMovieName] = useState('');
-  const[review, setReview] = useState('');
-  const[movieReviewList, setMovieList] = useState([])
+  // const[movie_name, setMovieName] = useState('');
+  // const[minute_runtime, setReview] = useState('');
+  const[movieList, setMovieList] = useState([]);
+  // var movieNameText;
+  // var minuteRuntimeText;
+  // var airDate;
+  // var price;
+  // var dateUpdated;
+  // var dateCreated;
+
 
   useEffect(()=>{
     Axios.get('http://localhost:3001/api/get').then((response)=> {
@@ -16,64 +23,105 @@ function App() {
     })
   }, [])
 
-  const submitReview = () => {
-    Axios.post('http://localhost:3001/api/insert', {
-      movie_name: movie_name, 
-      movie_review: review,
-    }).then(() => {
-      alert("successful insert");
-    })
-  };
+  // const submitReview = () => {
+  //   Axios.post('http://localhost:3001/api/insert', {
+  //     movie_name: movie_name, 
+  //     minute_runtime: minute_runtime,
+  //   }).then(() => {
+  //     alert("successful insert");
+  //   })
+  // };
 
   return <div className="App">
-    <h1>CRUD APP v1</h1>
+    <h1>Database Form</h1>
+    <div className="horizontalContainer">
 
-    <div className="form">
-      <label>Movie Name:</label>
-      <input type="text" name="movieName" onChange={(e) => {
-        setMovieName(e.target.value)
-      }}/>
-      <label>Review:</label>
-      <input type="text" name="review" onChange={(e) => {
-        setReview(e.target.value)
-      }}/>
-
-      <button onClick={submitReview}>Submit</button>
-      {movieReviewList.map((val)=> {
-        return <h1>
-          MovieName: {val.movie_name} | MovieReview: {val.movie_review}
-          </h1>
-      })}
-
-      <div className="buttonContainer">
-        <button onClick={() => { first()}} >First</button>
-        <button onClick={() => { previous()} }>Previous</button>
-        <button onClick={() => { next()} }>Next</button>
-        <button onClick={() => { last()} }>Last</button>
+      <div className="verticalContainer">
+            <h1 className="label">Movie Name</h1>
+            <h1 className="label">Minute Runtime</h1>
+            <h1 className="label">Air Date</h1>
+            <h1 className="label">Price</h1>
+            <h1 className="label">Date Modified</h1>
+            <h1 className="label">Date Created</h1>
       </div>
+
+      <div className="verticalContainer">
+            <text className="response" id="movie name"/>
+            <text className="response" id="minute runtime"/>
+            <text className="response" id="air date"/>
+            <text className="response" id="price"/>
+            <text className="response" id="date modified"/>
+            <text className="response" id="date created"/>
+      </div>
+
+    </div>
+    
+
+    <div className="buttons">
+      <button onClick={() => { first()}} >First</button>
+      <button onClick={() => { previous()} }>Previous</button>
+      <button onClick={() => { next()} }>Next</button>
+      <button onClick={() => { last()} }>Last</button>
     </div>
   </div>
   ;
 
+  
+  function updateText(){
+    var selectedMovie = getById(index);
+
+    document.getElementById("movie name").innerHTML = selectedMovie.movie_name;
+    document.getElementById("minute runtime").innerHTML = selectedMovie.minute_runtime;
+    document.getElementById("air date").innerHTML = selectedMovie.air_date;
+    document.getElementById("price").innerHTML = selectedMovie.price;
+    document.getElementById("date modified").innerHTML = selectedMovie.date_modified;
+    document.getElementById("date created").innerHTML = selectedMovie.date_created;
+  }
+  // function updateMovieName(){
+  //   document.getElementById("movie name").innerHTML = getById(index).movie_name;
+  // }
+  // function updateMinuteRuntime(){
+  //   document.getElementById("minute runtime").innerHTML = getById(index).minute_runtime;
+  // }
+  // function updateAirDate(){
+  //   document.getElementById("air date").innerHTML = getById(index).air_date;
+  // }
+
+  function getById(num){
+    var response;
+    movieList
+      .map((val, listIndex) => {
+        if(listIndex === num){
+          console.log("index of: " + listIndex + " for val " + val.movie_name)
+          response = val;
+        }
+      })
+      return response;
+  }
+
   function first(){
     index = 0;
     console.log("index=" + index);
+    updateText()
   }
   function previous(){
     if(index > 0){
       index--;
       console.log("index=" + index);
+      updateText()
     }
   }
   function next(){
-    if(index < 10) {
+    if(index < movieList.length - 1) {
       index++;
       console.log("index=" + index);
+      updateText()
     }
   }
   function last(){
-    index = 9;
+    index = movieList.length - 1;
     console.log("index=" + index);
+    updateText()
   }
 }
 
--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const db = mysql.createPool({
     host: "localhost",
     user: "root",
     password: "",
-    database: "crudDB",
+    database: "project",
     port:3306,
 });
 
@@ -17,24 +17,24 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/api/get", (req, res)=> {
-    const sqlSelect = "SELECT * FROM movie_reviews;"
+    const sqlSelect = "SELECT * FROM movie;"
     db.query(sqlSelect, (err, result) => {
         res.send(result);
     })
 })
 
-app.post("/api/insert", (req, res)=> {
+// app.post("/api/insert", (req, res)=> {
 
-    const movie_name = req.body.movie_name
-    const movie_review = req.body.movie_review
+//     const movie_name = req.body.movie_name
+//     const minute_runtime = req.body.minute_runtime
 
-    const sqlInsert = "INSERT INTO movie_reviews (movie_name, movie_review) VALUES (?, ?)"
-    db.query(sqlInsert, [movie_name, movie_review], (err, result) => {
-        console.log(result);
-    })
-});
+//     const sqlInsert = "INSERT INTO movie (movie_name, minute_runtime) VALUES (?, ?)"
+//     db.query(sqlInsert, [movie_name, minute_runtime], (err, result) => {
+//         console.log(result);
+//     })
+// });
  
-    // const sqlInsert = "SELECT * FROM movie_reviews;"
+    // const sqlInsert = "SELECT * FROM movie;"
     // db.query(sqlInsert, (err, result) => {
     //     res.send("Result: " + result[0].movie_name);
     // })
